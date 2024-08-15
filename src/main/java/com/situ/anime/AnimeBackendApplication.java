6baa299e707dbb450433b7a099b73651c54fac73
嘉然今天吃什么 <10677796+DianaEatArcstarToday@user.noreply.gitee.com>
2024-08-15 14:06:42 +0800

    Initial commit

---
+++ src/main/java/com/situ/anime/AnimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.situ.anime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/situ/anime/AnimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.situ.anime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
