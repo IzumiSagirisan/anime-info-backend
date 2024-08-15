cae1e8e8e2830eb0e96110a10836cec65fa664cf
嘉然今天吃什么 <10677796+DianaEatArcstarToday@user.noreply.gitee.com>
2024-08-15 14:09:53 +0800

    后端项目创建

--- src/main/java/com/situ/anime/AnimeBackendApplication.java
+++ src/main/java/com/situ/anime/AnimeBackendApplication.java
@@ -3,11 +3,14 @@ package com.situ.anime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author liangyunfei
+ */
 @SpringBootApplication
 public class AnimeBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimeBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeBackendApplication.class, args);
+    }
 
 }
